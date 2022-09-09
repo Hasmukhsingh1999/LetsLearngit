d46849358a67bff91fa4908fba67ac843bd33ded
Hasmukh Singh <hasmukhsingh90@gmail.com>
2022-09-09 20:32:57 +0530

    addes message in sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -1,8 +1,9 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    int a=9,b=20,sum;
-    sum = a+b;
-    cout<<sum;
+int main()
+{
+    int a = 9, b = 20, sum;
+    sum = a + b;
+    cout << "The sum of a and b is  :" << sum;
 }
